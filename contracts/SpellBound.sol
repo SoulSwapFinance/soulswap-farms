47557daeb1eafee9c3bc316919295dea3c33b0bd
Buns <bunsthedev@gmail.com>
2021-07-25 02:49:59 -0500

    update: SpellBound.sol

--- contracts/SpellBound.sol
+++ contracts/SpellBound.sol
@@ -1,38 +1,56 @@
 // SPDX-License-Identifier: MIT
 
-import '@openzeppelin/contracts/access/Ownable.sol';
-import './libs/ERC20.sol';
-import '@openzeppelin/contracts/utils/Address.sol';
-
 pragma solidity ^0.8.0;
 
-contract SpellBound is ERC20("SpellBound", "SPELL"), Ownable {
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+// spell is the neatest bound around. come in with some soul, and leave with some more! 
+// handles swapping to and from spell -- our dex reward token.
+
+contract SpellBound is ERC20("SpellBound", "SPELL"), Ownable, ReentrancyGuard {
     IERC20 public soul;
+    bool isInitialized; // stores whether contract has been initialized
 
-    constructor(IERC20 _soul) {
+    // the soul token contract
+    function initialize(IERC20 _soul) external onlyOwner {
         soul = _soul;
+
     }
 
-    // Enter the bound (somewhere lost between the simulacra and simulation). 
-    // Pay some SOUL. Collect some shares.
-    function enter(uint256 _amount) public {
-        uint256 totalSoul = soul.balanceOf(address(this));
-        uint256 totalShares = totalSupply();
-        if (totalShares == 0 || totalSoul == 0) {
-            _mint(msg.sender, _amount);
-        } else {
-            uint256 what = _amount * (totalShares) / (totalSoul);
-            _mint(msg.sender, what);
+    // locks soul, mints spell at bound rate
+    function enter(uint soulQty) public nonReentrant {
+        require(isInitialized, 'staking has not yet begun');
+        // acquires soul locked in the contract
+        uint totalSoul = soul.balanceOf(address(this));
+        // acquires spellBound in existence
+        uint totalBound = totalSupply();
+        // if no spellBound exists, mint it 1:1 to soul recieves
+        if (totalBound == 0 || totalSoul == 0) { _mint(msg.sender, soulQty); }
+        // calc & mint spellBound qty the soul is worth
+        // the ratio will change overtime
+            // spellBound is burned / minted
+            // soul fees deposited + deposited soul + withdrawn soul
+        else {
+            uint boundRate = totalBound / totalSoul; // qty of bound : quantity of soul in contract
+            uint boundQty = soulQty * boundRate;
+            _mint(msg.sender, boundQty);
         }
-        soul.transferFrom(msg.sender, address(this), _amount);
+        // locks the soul in the contract
+        soul.transferFrom(msg.sender, address(this), soulQty);
     }
 
-    // Leave the Bound? 
-    // => Claim Back Your SOUL.
-    function leave(uint256 _share) public {
-        uint256 totalShares = totalSupply();
-        uint256 what = _share * (soul.balanceOf(address(this))) / (totalShares);
-        _burn(msg.sender, _share);
-        soul.transfer(msg.sender, what);
+    // leaves the spellbound. reclaims soul.
+    // unlocks soul rewards + staked | burns bounded spell
+    function leave(uint spellQty) public nonReentrant {
+        // qty of spellBound in existence
+        uint totalBound = totalSupply();
+        // calcs the qty of soul the spell is worth
+        uint boundRate = soul.balanceOf(address(this)) / totalBound;
+        uint rewards = spellQty * boundRate;
+        _burn(msg.sender, spellQty);
+        soul.transfer(msg.sender, rewards);
     }
 }
