6f6a75cbe99eee15f95f885944436af01b4294a6
Buns <bunsthedev@gmail.com>
2021-09-14 14:34:55 -0500

    share percentages: passing (6/1/1)

--- test/SoulSummoner.test.js
+++ test/SoulSummoner.test.js
@@ -54,11 +54,15 @@ describe('SoulSummoner', () => {
     console.log('my address: %s', buns)
 
     // approve, and mint lp and burn excess soul
-    await soul.approve(summoner.address, toWei(10000))
-    await lpToken.approve(summoner.address, toWei(10000))
-    lpToken.mint(toWei(10000))
-    console.log('minted %s tokens', 10000)
-    soul.burn(toWei(49990000))
+    await soul.approve(summoner.address, toWei(100_000))
+    await lpToken.approve(summoner.address, toWei(100_000))
+
+    // mint lpToken
+    lpToken.mint(toWei(100_000))
+    console.log('minted %s tokens', 100_000)
+
+    // burn excess SOUL
+    soul.burn(toWei(49_900_000))
     console.log('burned excess tokens')
 
     team = '0x81Dd37687c74Df8F957a370A9A4435D873F5e5A9'
@@ -70,7 +74,7 @@ describe('SoulSummoner', () => {
     console.log('team: %s', team)
     
     // [user] enter staking
-    await summoner.enterStaking(toWei(10000))
+    await summoner.enterStaking(toWei(100_000))
     soulUserStaked = await soul.balanceOf(summoner.address)
     console.log('[user] staked %s SOUL', fromWei(soulUserStaked))
   })
@@ -79,7 +83,7 @@ describe('SoulSummoner', () => {
     describe('deposits / withdraws', function() {
         it('should return 10,000 SOUL in the summoner', async function() {
           console.log('summoner soul bal', fromWei(soulUserStaked))
-          expect(await soul.balanceOf(summoner.address)).to.equal(toWei(10000))
+          expect(await soul.balanceOf(summoner.address)).to.equal(toWei(100_000))
         })
     })
 
@@ -101,19 +105,26 @@ describe('SoulSummoner', () => {
 
     // withdraw and allocate
     describe('withdraw staked soul', function() {
-      it('should return soul balance + rewards balance', async function() {
+      it('should return pending rewards of 250K', async function() {
         increaseTime(86400) // 1 day
         pendingRewards = await summoner.pendingSoul(0, buns)
         console.log('pending soul %s: ', fromWei(pendingRewards))
         expect(await summoner.pendingSoul(0, buns)).to.equal(pendingRewards)
-        
+      })
+
+      it('should return [summoner] balance of 10,000 SOUL', async function() {
+        increaseTime(86400) // 1 day
+        pendingRewards = await summoner.pendingSoul(0, buns)
         dao = await summoner.dao()
         team = await summoner.team()
 
         summonerSoulBalance = await soul.balanceOf(summoner.address)
         console.log('[summoner] balance: %s SOUL', fromWei(summonerSoulBalance))
-        expect(await soul.balanceOf(summoner.address)).to.equal(summonerSoulBalance)
+        expect(await soul.balanceOf(summoner.address)).to.equal(toWei(100_000))
+      })
         
+      it('should return total payout of ~250K SOUL', async function() {
+        increaseTime(86400) // 1 day
         preUserSoul = await soul.balanceOf(buns)
         preDaoSoul = await soul.balanceOf(dao)
         preTeamSoul = await soul.balanceOf(team)
@@ -123,30 +134,28 @@ describe('SoulSummoner', () => {
         console.log('[team] pre-withdrawal balance: %s SOUL', fromWei(preTeamSoul))
         
         // leave staking
-        await summoner.leaveStaking(toWei(10000))
-        console.log('left staking')
+        unstakedAmount = toWei(100_000)
+        await summoner.leaveStaking(unstakedAmount)
+        console.log('[user] withdrew: %s SOUL', 100_000)
+        soul.burn(toWei(100_000)) // destroys for easy maths
 
         newUserSoul = await soul.balanceOf(buns)
         newDaoSoul = await soul.balanceOf(dao)
         newTeamSoul = await soul.balanceOf(team)
         
-        // calculate and log payouts //
+        //* calculate and log payouts *//
 
         // [user] payout
-        soulUserPayout = await newUserSoul.sub(preUserSoul)
-        await soulUserPayout
-        console.log('user paid out: %s SOUL', fromWei(soulUserPayout))
-        
+        soulUserPayout = newUserSoul.sub(preUserSoul)
+        console.log('[user] rewarded: %s SOUL', fromWei(soulUserPayout))
         // [dao] payout
         soulDaoPayout = await newDaoSoul.sub(preDaoSoul)
-        await soulDaoPayout
-        console.log('user paid out: %s SOUL', fromWei(soulDaoPayout))
-        
+        console.log('[dao] sent: %s SOUL', fromWei(soulDaoPayout))
         // [team] payout
         soulTeamPayout = await newTeamSoul.sub(preTeamSoul)
-        await soulTeamPayout
-        console.log('user paid out: %s SOUL', fromWei(soulTeamPayout))
+        console.log('[team] sent: %s SOUL', fromWei(soulTeamPayout))
 
+        // [total] payout
         totalPayoutOneDay = await
           soulUserPayout
             .add(soulDaoPayout)
@@ -154,8 +163,33 @@ describe('SoulSummoner', () => {
       
         console.log('total payout: %s', fromWei(totalPayoutOneDay))
         supply = await soul.totalSupply()
-        console.log('supply; %s SOUL', fromWei(supply))
+        console.log('total 1D supply; %s SOUL', fromWei(supply))
+
+        // [total] payouts == total supply
+        expect(await totalPayoutOneDay).to.equal(supply)
+
       })
+
+      it('[shares] should return: 6/8 user, 1/8 dao, 1/8 dev', async function() {
+        userShare = soulUserPayout.mul(1000).div(totalPayoutOneDay)
+        rawDaoShare = soulDaoPayout.mul(1000).div(totalPayoutOneDay)
+        rawTeamShare = soulTeamPayout.mul(1000).div(totalPayoutOneDay)
+
+        // adjust by rounding
+        daoShare = rawDaoShare.add(1)
+        teamShare = rawTeamShare.add(1)
+
+        // log user shares
+        console.log('user share: %s', userShare)
+        console.log('dao share: %s', daoShare)
+        console.log('team share: %s', teamShare)
+        
+        // expect the shares to align with tokenomics
+        expect(await userShare.add(daoShare).add(teamShare)).to.equal(1000)
+        expect(await userShare).to.equal(750)
+        expect(await daoShare).to.equal(125)
+        expect(await teamShare).to.equal(125)
     })
+  })
 })
 
